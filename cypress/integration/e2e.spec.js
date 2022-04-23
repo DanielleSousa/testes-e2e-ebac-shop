67ae6a093662f41e9cb6b43117bbb3775afb36f3
“Danielle <danielle.msousa@hotmail.com>
2022-04-23 16:29:43 -0300

    teste e2e usando custom

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -11,53 +11,26 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         E validando minha compra ao final */
 
 
-    beforeEach(() => {
-         cy.visit("/produtos/")
-    });
-        
+        beforeEach(() => {
+            cy.visit('minha-conta')
+            cy.fixture('perfil').then(dados => {
+                cy.login(dados.usuario, dados.senha)
+                cy.visit('/produtos/')
+            
+            })
+            
+       });
+           
+       it('Deve fazer o fluxo de compras',()=> {
+           cy.comprar()
+           cy.get('.woocommerce-message > .button').click()
+           cy.get('.checkout-button').click()
+           EntregaPage.dadosdeEntregaDaCompra('Danielle', 'Sousa', 'Magalu', 'Brasil', 'Rua Afonso Pena', '2022', 'Barueri', 'São Paulo', '06435000', '11-98765-4321', 'daniellesousa@magalu.com')
+           cy.get('.woocommerce-notice').should('contain','Obrigado. Seu pedido foi recebido.')
+       
+    
+      
 
-    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        cy.get('[class="product-block grid"]')
-        .last()
-        .click()
-        cy.get('.button-variable-item-XS').click()
-        cy.get(':nth-child(2) > .value > .variable-items-wrapper > .variable-item').click()
-        cy.get('.input-text').clear().type('1')
-        cy.get('.single_add_to_cart_button').click()
-        cy.visit("/produtos/")
-        cy.get('[class="product-block grid"]')
-        .first()
-        .click()
-        cy.get('.button-variable-item-XS').click()
-        cy.get('.button-variable-item-Red').click()
-        cy.get('.input-text').clear().type('1')
-        cy.get('.single_add_to_cart_button').click()
-        cy.visit("/produtos/")
-        cy.get(':nth-child(2) > .page-numbers').click()
-        cy.get('[class="product-block grid"]')
-        .first()
-        .click()
-        cy.get('.button-variable-item-XS').click()
-        cy.get('.button-variable-item-Black').click()
-        cy.get('.input-text').clear().type('1')
-        cy.get('.single_add_to_cart_button').click()
-        cy.visit("/produtos/")
-        cy.get(':nth-child(3) > .page-numbers').click()
-        cy.get('[class="product-block grid"]')
-        .first()
-        .click()
-        cy.get('.single_add_to_cart_button').click()
-        cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
-        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
-        cy.get('.showlogin').click()
-        cy.get('#username').type('aluno_ebac@teste.com')
-        cy.get('#password').type('teste@teste.com')
-        cy.get('.woocommerce-button').click()
-        EntregaPage.dadosdeEntregaDaCompra('Danielle', 'Sousa', 'Magalu', 'Brasil', 'Rua Afonso Pena', '2022', 'Barueri', 'São Paulo', '06435000', '11-98765-4321', 'daniellesousa@magalu.com')
-        cy.get('.woocommerce-notice').should('contain','Obrigado. Seu pedido foi recebido.')
-    
     });
-    
-  
-        
 });
+    
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,7 +27,23 @@
 
 Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, {log: false})
+    cy.get('#password').type(senha)
     cy.get('.woocommerce-form > .button').click()
 });
 
+
+
+Cypress.Commands.add('comprar',() => {
+    var quantidade = 4
+
+    cy.get('[class="product-block grid"]')
+    .last()
+    .click()
+    cy.get('.button-variable-item-L').click()
+    cy.get('.button-variable-item-Blue').click()
+    cy.get(".input-text").clear().type(quantidade)
+    cy.get('.single_add_to_cart_button').click()
+
+}); 
+
+ 
